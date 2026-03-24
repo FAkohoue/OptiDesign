6b67700a0db4cdd8cd00cde4aacbeb2e52090f84
Félicien Akohoue <akohoue.f@gmail.com>
2026-03-24 15:54:23 -0500

    update

--- scripts/build.R
+++ scripts/build.R
@@ -4,17 +4,61 @@
 source("data-raw/generate_example_data.R")
 devtools::document()
 devtools::check()
-devtools::build()
 
-
-devtools::document()
 devtools::install()
-devtools::check()
+
 
 unloadNamespace("OptiDesign")
 pkgdown::build_site()
 
+devtools::build()
+
 devtools::build_vignettes()
 pkgdown::build_site()
 
-file.exists(".github/workflows/pkgdown.yaml")
+install.packages("rsvg")
+rsvg::rsvg_png(
+  "man/figures/logo.svg",
+  "man/figures/logo.png",
+  width  = 1360,
+  height = 560
+)
+
+# Then build favicons from the PNG instead
+pkgdown::build_favicons(overwrite = TRUE)
+
+options(timeout = 3000)  # 5 minutes
+pkgdown::build_favicons(overwrite = TRUE)
+
+# Regenerate favicons from the correct logo
+pkgdown::build_favicons(overwrite = TRUE)
+pkgdown::build_site()
+
+
+library(magick)
+
+# Load your logo
+img <- image_read("man/figures/logo.png")
+
+# Create favicon sizes
+sizes <- c(16, 32, 48, 64, 180, 192, 512)
+
+dir.create("pkgdown/assets", recursive = TRUE, showWarnings = FALSE)
+
+for (s in sizes) {
+  resized <- image_resize(img, paste0(s, "x", s))
+  image_write(resized, paste0("pkgdown/assets/favicon-", s, ".png"))
+}
+
+# Create favicon.ico (multi-size)
+ico <- image_resize(img, "64x64")
+image_write(ico, "pkgdown/assets/favicon.ico")
+
+writeLines(
+  c(
+    '<link rel="icon" type="image/svg+xml" href="logo.svg">',
+    '<link rel="icon" type="image/png" sizes="32x32" href="favicon-32.png">',
+    '<link rel="apple-touch-icon" href="favicon-180.png">'
+  ),
+  "pkgdown/assets/favicon.html"
+)
